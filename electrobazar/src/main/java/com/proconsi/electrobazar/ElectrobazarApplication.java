8ab8f3563403cf257f9bf1a28d4f44c730622f7a
Samuel.Reyero <samuel.reyero@proconsi.es>
2026-02-23 13:48:14 +0100

    Estructura del proyecto

---
+++ electrobazar/src/main/java/com/proconsi/electrobazar/ElectrobazarApplication.java
@@ -0,0 +1,13 @@
+package com.proconsi.electrobazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectrobazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectrobazarApplication.class, args);
+	}
+
+}
---
+++ electrobazar/src/test/java/com/proconsi/electrobazar/ElectrobazarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proconsi.electrobazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectrobazarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
