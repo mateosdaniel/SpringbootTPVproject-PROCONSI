2ec4fcce27a2506ac40ec06c871b5f633a1aea73
mateosdaniel <danielmateos684@gmail.com>
2026-02-25 13:22:09 +0100

    root user fixed

--- electrobazar/src/main/java/com/proconsi/electrobazar/ElectrobazarApplication.java
+++ electrobazar/src/main/java/com/proconsi/electrobazar/ElectrobazarApplication.java
@@ -14,13 +14,14 @@ public class ElectrobazarApplication {
 	public org.springframework.boot.CommandLineRunner initData(
 			com.proconsi.electrobazar.service.WorkerService workerService) {
 		return args -> {
-			if (workerService.findAll().isEmpty()) {
+			if (workerService.findAll().stream().noneMatch(w -> "root".equals(w.getUsername()))) {
 				com.proconsi.electrobazar.model.Worker defaultWorker = new com.proconsi.electrobazar.model.Worker();
 				defaultWorker.setUsername("root");
 				defaultWorker.setPassword("root");
 				defaultWorker.setActive(true);
 				defaultWorker.setPermissions(java.util.Set.of("MANAGE_PRODUCTS_TPV", "CASH_CLOSE", "ADMIN_ACCESS"));
 				workerService.save(defaultWorker);
+				System.out.println(">>> Usuario ROOT creado por defecto (root/root)");
 			}
 		};
 	}
